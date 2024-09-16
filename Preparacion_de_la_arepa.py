a0310aa1947f1e70a70f74c9f2d311fc47fa3193
Alejanro0309 <marcano.alejandro@correo.unimet.edu.ve>
2024-09-15 22:07:20 -0400

    subiendo primera tarea

---
+++ Preparacion_de_la_arepa.py
@@ -0,0 +1,13 @@
+#Ingredientes de la arepa 
+
+agua=1 #tasa de agua
+harina=1 #tasa de harina
+sal=0.20 #tasa de sal
+aceite=0.06 #tasa de aceite
+
+#se hace la mezcla
+bol=agua+harina+sal+aceite
+budare=bol*1.1 #cuando se hace la arepa aumenta de volumen
+arepa=budare
+
+print("el volumen de la arepa va hacer:",arepa,"gr")
